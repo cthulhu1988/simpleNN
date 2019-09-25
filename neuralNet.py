e8805cecee2944af4248ad1dfb07344c9c879e3e
loki <loki@pop-os.localdomain>
2019-09-24 20:27:54 -0500

    started writing class

---
+++ neuralNet.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+
+class NeuralNet():
+
+    def__init__(self):
+        np.random.seed(1)
+        self.synaptic_weights = np.random.random((3,1)) -1
+
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
